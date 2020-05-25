7f82e7c4ec63b898c9179863b4c721b8a532ba70
xinyagithub <xinya.zhu@aspose.com>
2020-05-25 11:35:55 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/xinyagithub/jusinyahtest
+
+go 1.13
---
+++ hello.go
@@ -0,0 +1,22 @@
+
+package pkg
+ 
+import "fmt"
+ 
+// Hello says hello.
+func Hello() {
+	fmt.Println("Hello go mod!")
+}
+ 
+// Bye says bye.
+func Bye() {
+	fmt.Println("Bye go mod!")
+}
+
+// Foo says Fuck.  
+///123
+func Foo() {
+	fmt.Println("Foo go mod!")
+	fmt.Println("---v2.0---")
+}
+ 

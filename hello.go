ae3d7ba455200ad51cc91b5b2b99343c54b061bc
xinyagithub <xinya.zhu@aspose.com>
2020-05-26 16:01:16 +0800

    v2.0.0

--- hello.go
+++ hello.go
@@ -2,21 +2,8 @@
 package pkg
  
 import "fmt"
- 
-// Hello says hello.
-func Hello() {
-	fmt.Println("Hello go mod!")
-}
- 
-// Bye says bye.
-func Bye() {
-	fmt.Println("Bye go mod!")
-}
 
-// Foo says Fuck.  
-///123
-func Foo1() {
-	fmt.Println("Foo go mod!")
-	fmt.Println("---v2.0---")
+func Foo() {
+	fmt.Println("---v2.0.0---")
 }
  
